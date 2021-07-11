3236c11176676b133037b28e68b27a9157218e8c
iMMoCodes <matti-makela@hotmail.com>
2021-07-12 00:22:44 +0300

    created middlewares

--- server/index.js
+++ server/index.js
@@ -6,12 +6,23 @@ const reqPath = path.join(__dirname, '../data/tour-single.json')
 const app = express()
 
 app.use(express.json())
+app.use((req, res, next) => {
+  console.log('hello from middleware')
+  next()
+})
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString()
+  next()
+})
+
 const tours = JSON.parse(fs.readFileSync(`${reqPath}`))
 
 // GET ALL TOURS
 const getAllTours = (req, res) => {
+  console.log(req.requestTime)
   res.status(200).json({
     status: 'success',
+    requestAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
