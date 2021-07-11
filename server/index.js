fbf8ab6989ba3e633ea37e39b5adc54bf7ebd311
iMMoCodes <matti-makela@hotmail.com>
2021-07-11 21:02:41 +0300

    Created a route to post a new tour

--- server/index.js
+++ server/index.js
@@ -4,8 +4,11 @@ const express = require('express')
 const reqPath = path.join(__dirname, '../data/tour-single.json')
 
 const app = express()
+
+app.use(express.json())
 const tours = JSON.parse(fs.readFileSync(`${reqPath}`))
 
+// GET REQUEST
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -13,6 +16,23 @@ app.get('/api/v1/tours', (req, res) => {
     data: {
       tours,
     },
+  })
+})
+
+// POST REQUEST
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newId }, req.body)
+
+  tours.push(newTour)
+
+  fs.writeFile(reqPath, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    })
   })
 })
 
