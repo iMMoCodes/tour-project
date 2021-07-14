0a53827afec7d76501db876ae166f810da18b4be
iMMoCodes <matti-makela@hotmail.com>
2021-07-15 02:22:03 +0300

    added enviroment variables

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+const dotenv = require('dotenv')
+dotenv.config({path: './config.env'})
 const app = require('./app')
 
 // START SERVER
-const port = 5000
+const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`)
 })
