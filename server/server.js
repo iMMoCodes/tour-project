79fe491893032ced87bfc6288160fbafb7e16536
iMMoCodes <matti-makela@hotmail.com>
2021-07-15 14:06:11 +0300

    Created a mongoose schema for a tour

--- server/server.js
+++ server/server.js
@@ -16,6 +16,24 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'))
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+})
+
+const Tour = mongoose.model('Tour', tourSchema)
+
 // START SERVER
 const port = process.env.PORT || 5000
 app.listen(port, () => {
