05ac9e01488b95606ff020a6b2ccc79434245372
iMMoCodes <matti-makela@hotmail.com>
2021-07-18 18:54:22 +0300

    refactored query methods into more reusable form

--- server/controllers/tourController.js
+++ server/controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel')
+const APIFeatures = require('../utils/apiFeatures')
 
 // TOP TOURS
 exports.aliasTopTours = (req, res, next) => {
@@ -10,50 +11,14 @@ exports.aliasTopTours = (req, res, next) => {
 
 // GET ALL TOURS
 exports.getAllTours = async (req, res) => {
-  console.log(req.query)
   try {
-    // BUILD QUERY
-    // FILTERING
-    const queryObj = { ...req.query }
-    const excludedFields = ['page', 'sort', 'limit', 'fields']
-    excludedFields.forEach((el) => delete queryObj[el])
-
-    // FILTERING WITH GTE, GT, LTE, LT
-    let queryStr = JSON.stringify(queryObj)
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
-
-    let query = Tour.find(JSON.parse(queryStr))
-
-    // SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ')
-      query = query.sort(sortBy)
-    } else {
-      query = query.sort('-createdAt')
-    }
-
-    // FIELD LIMITING
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ')
-      query = query.select(fields)
-    } else {
-      query = query.select('-__v')
-    }
-
-    // PAGINATION
-    const page = req.query.page * 1 || 1
-    const limit = req.query.limit * 1 || 100
-    const skip = (page - 1) * limit
-
-    query = query.skip(skip).limit(limit)
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments()
-      if (skip >= numTours) throw new Error('This page does not exist.')
-    }
-
     // EXECUTE QUERY
-    const tours = await query
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate()
+    const tours = await features.query
 
     // SEND RESPONSE
     res.status(200).json({
