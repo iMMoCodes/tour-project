7879bc655543f2d46df6fc24c0b2584fc252309f
iMMoCodes <matti-makela@hotmail.com>
2021-07-18 18:15:37 +0300

    added an alias route for top 5 tours

--- server/controllers/tourController.js
+++ server/controllers/tourController.js
@@ -1,5 +1,13 @@
 const Tour = require('../models/tourModel')
 
+// TOP TOURS
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5'
+  req.query.sort = '-ratingsAverage,price'
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty'
+  next()
+}
+
 // GET ALL TOURS
 exports.getAllTours = async (req, res) => {
   console.log(req.query)
