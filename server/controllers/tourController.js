cb99165f9e5535863dc808163fb08a0c4b61877a
iMMoCodes <matti-makela@hotmail.com>
2021-07-18 15:29:12 +0300

    added greater than and less than operators to filtering

--- server/controllers/tourController.js
+++ server/controllers/tourController.js
@@ -3,8 +3,22 @@ const Tour = require('../models/tourModel')
 // GET ALL TOURS
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find()
+    // BUILD QUERY
+    // FILTERING
+    const queryObj = { ...req.query }
+    const excludedFields = ['page', 'sort', 'limit', 'fields']
+    excludedFields.forEach((el) => delete queryObj[el])
 
+    // FILTERING WITH GTE, GT, LTE, LT
+    let queryStr = JSON.stringify(queryObj)
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
+
+    const query = Tour.find(JSON.parse(queryStr))
+
+    // EXECUTE QUERY
+    const tours = await query
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
