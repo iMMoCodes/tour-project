f5f3e650e2eebc91460eea2f0a862a0da6f5bbc9
iMMoCodes <matti-makela@hotmail.com>
2021-08-06 01:10:41 +0300

    handled different errors in controller and checking for unhandled and uncaught errors

--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION. Shutting down...')
+  console.log(err.name, err.message)
+  process.exit(1)
+})
+
 const app = require('./app')
 
 const DB = process.env.DATABASE.replace(
@@ -18,6 +25,14 @@ mongoose
 
 // START SERVER
 const port = process.env.PORT || 5000
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is running on port ${port}...`)
+})
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION. Shutting down...')
+  console.log(err.name, err.message)
+  server.close(() => {
+    process.exit(1)
+  })
 })
