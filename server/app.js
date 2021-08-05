3b33db137a206efd27d5fd05f3af6d2edc45bff9
iMMoCodes <matti-makela@hotmail.com>
2021-08-05 22:08:34 +0300

    handled routes that doesn't match known routes

--- server/app.js
+++ server/app.js
@@ -7,15 +7,11 @@ const userRouter = require('./routes/userRoutes')
 const app = express()
 
 // MIDDLEWARES
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
-app.use(express.json())
 
-app.use((req, res, next) => {
-  console.log('hello from middleware')
-  next()
-})
+app.use(express.json())
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
@@ -25,5 +21,12 @@ app.use((req, res, next) => {
 // ROUTES
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server.`,
+  })
+})
 
 module.exports = app
