640729c97256d9f62b93a8aeca109aa6b2d5c226
iMMoCodes <matti-makela@hotmail.com>
2021-08-26 15:34:56 +0300

    configured nodemailer to send emails and created reset password email logic

---
+++ server/utils/email.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer')
+
+const sendEmail = async (options) => {
+  // Create a transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  })
+  // Define the email options
+  const mailOptions = {
+    from: 'Immo <immo@immo.fi>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  }
+  // Actually send email
+  await transporter.sendMail(mailOptions)
+}
+
+module.exports = sendEmail
