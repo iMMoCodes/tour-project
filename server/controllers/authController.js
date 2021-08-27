e89bf68b573f6e7eee49e61b2549600c9bcbc906
iMMoCodes <matti-makela@hotmail.com>
2021-08-27 16:00:00 +0300

    created reset password logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 const { promisify } = require('util')
+const crypto = require('crypto')
 const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 const catchAsync = require('../utils/catchAsync')
@@ -141,4 +142,33 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 })
 
-exports.resetPassword = (req, res, next) => {}
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // Get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex')
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  })
+  // If token has not expired and there's user, set new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired.', 400))
+  }
+  user.password = req.body.password
+  user.passwordConfirm = req.body.passwordConfirm
+  user.passwordResetToken = undefined
+  user.passwordResetExpires = undefined
+  await user.save()
+  // Update changedPasswordAt property for user
+
+  // Log the user in, send JWT
+  const token = signToken(user._id)
+
+  res.status(200).json({
+    status: 'success',
+    token,
+  })
+})
