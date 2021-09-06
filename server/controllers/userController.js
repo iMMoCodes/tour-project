77e8892c1ed08ad90221ef1b21b6bbbef2ecd3ad
iMMoCodes <matti-makela@hotmail.com>
2021-09-07 02:32:13 +0300

    User can now update his name and email

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('../models/userModel')
 const catchAsync = require('../utils/catchAsync')
+const AppError = require('../utils/appError')
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {}
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el]
+  })
+  return newObj
+}
 
 // GET ALL USERS
 exports.getAllUsers = catchAsync(async (req, res, next) => {
@@ -11,6 +20,32 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     results: users.length,
     data: {
       users,
+    },
+  })
+})
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // Crete error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password update. Please use /updateMyPassword',
+        400
+      )
+    )
+  }
+  // Filter out unwanted field names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email')
+  // Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  })
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
     },
   })
 })
