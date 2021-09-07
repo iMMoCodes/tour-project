3e813da14d6d40d293b896dd6506b4257f73a8a3
iMMoCodes <matti-makela@hotmail.com>
2021-09-07 23:34:03 +0300

    modelled locations and tour guides

--- server/models/tourModel.js
+++ server/models/tourModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify')
+// const User = require('./userModel')
+// const validator = require('validator')
 
 const tourSchema = new mongoose.Schema(
   {
@@ -76,6 +78,36 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -92,6 +124,12 @@ tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true })
   next()
 })
+
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id))
+//   this.guides = await Promise.all(guidesPromises)
+//   next()
+// })
 
 // QUERY MIDDLEWARE
 tourSchema.pre(/^find/, function (next) {
