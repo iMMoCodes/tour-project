65e126225e8b89ddb6fe2b87421ffd1e968e81fb
iMMoCodes <matti-makela@hotmail.com>
2021-09-20 05:35:53 +0300

    created tour pages

--- server/controllers/viewsController.js
+++ server/controllers/viewsController.js
@@ -5,15 +5,23 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   const tours = await Tour.find()
   // 2) Build template
-  // 3) Render template using tour data from 1
+  // 3) Render template using tour data
   res.status(200).render('overview', {
     title: 'All Tours',
     tours,
   })
 })
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) Get the data for the requested tour
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  })
+  // 2) Build template
+  // 3) Render template using data
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour,
   })
-}
+})
