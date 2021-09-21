1c688724b146d3db8e51799a16c09961ddb9327e
iMMoCodes <matti-makela@hotmail.com>
2021-09-21 03:47:29 +0300

    created feature to login as a user in the front end and refactored code

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -75,6 +75,8 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1]
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt
   }
 
   if (!token) {
@@ -99,6 +101,30 @@ exports.protect = catchAsync(async (req, res, next) => {
   }
   // Grant access to protected route
   req.user = currentUser
+  next()
+})
+
+// Only for rendered pages, no errors!
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  if (req.cookies.jwt) {
+    // 1) verify token
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET
+    )
+    // Check if user still exists
+    const currentUser = await User.findById(decoded.id)
+    if (!currentUser) {
+      return next()
+    }
+    // Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next()
+    }
+    // There is a logged in user
+    res.locals.user = currentUser
+    return next()
+  }
   next()
 })
 
--- server/public/js/mapbox.js
+++ server/public/js/mapbox.js
@@ -1,46 +1,46 @@
-const locations = JSON.parse(document.getElementById('map').dataset.locations)
+export const displayMap = (locations) => {
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoiaW1tb21tbyIsImEiOiJja3RzMjhlMWMxYzE2MnZxdXNzZGRvZDNuIn0.bM8T0Qj9qdHYRALU2wuvZQ'
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiaW1tb21tbyIsImEiOiJja3RzMjhlMWMxYzE2MnZxdXNzZGRvZDNuIn0.bM8T0Qj9qdHYRALU2wuvZQ'
-
-let map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/immommo/ckts2ydw330y017o2neuuenm2',
-  scrollZoom: false,
-})
-
-const bounds = new mapboxgl.LngLatBounds()
-
-locations.forEach((loc) => {
-  // Create marker
-  const el = document.createElement('div')
-  el.className = 'marker'
-
-  // Add marker
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom',
+  let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/immommo/ckts2ydw330y017o2neuuenm2',
+    scrollZoom: false,
   })
-    .setLngLat(loc.coordinates)
-    .addTo(map)
 
-  // Add popup
-  new mapboxgl.Popup({
-    offset: 30,
+  const bounds = new mapboxgl.LngLatBounds()
+
+  locations.forEach((loc) => {
+    // Create marker
+    const el = document.createElement('div')
+    el.className = 'marker'
+
+    // Add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom',
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map)
+
+    // Add popup
+    new mapboxgl.Popup({
+      offset: 30,
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+      .addTo(map)
+
+    // Extend map bounds to include current location
+    bounds.extend(loc.coordinates)
   })
-    .setLngLat(loc.coordinates)
-    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-    .addTo(map)
 
-  // Extend map bounds to include current location
-  bounds.extend(loc.coordinates)
-})
-
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 100,
-    right: 100,
-  },
-})
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100,
+    },
+  })
+}
