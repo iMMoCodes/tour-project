cb4b39b0cbc9e531adc74f4ccec76fc9b916181e
iMMoCodes <matti-makela@hotmail.com>
2021-09-27 10:06:36 +0300

    creted email handler and email templates for welcome and reset password emails

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,7 +4,7 @@ const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
-const sendEmail = require('../utils/email')
+const Email = require('../utils/email')
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -48,6 +48,8 @@ exports.signUp = catchAsync(async (req, res, next) => {
     passwordResetToken: req.body.passwordResetToken,
     paasswordResetExpires: req.body.paasswordResetExpires,
   })
+  const url = `${req.protocol}://${req.get('host')}/me`
+  await new Email(newUser, url).sendWelcome()
   createSendToken(newUser, 201, res)
 })
 
@@ -162,17 +164,12 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const resetToken = user.createPasswordResetToken()
   await user.save({ validateBeforeSave: false })
   // Send back as email
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/users/resetPassword/${resetToken}`
-
-  const message = `Forgot your password? Submit a request with your new password and password confirm to: ${resetURL}.\nIf you didn't forgot your password, please ignore this email!`
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message,
-    })
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`
+    await new Email(user, resetURL).sendPasswordReset()
+
     res.status(200).json({
       status: 'success',
       message: 'Token sent to email.',
