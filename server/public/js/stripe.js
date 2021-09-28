de72fa0a618ca7da876613265dade16f247b607e
iMMoCodes <matti-makela@hotmail.com>
2021-09-28 08:57:11 +0300

    handled processing of payments on the front end

---
+++ server/public/js/stripe.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import { showAlert } from './alerts'
+const stripe = Stripe(
+  'pk_test_51JeEUDKnN53WRkukMnbqL8Xt4aTkymvo3dl4X0O8Gl6w0p2YFygaEhGnjw0F5whAhY3yxcQT2af9tbnbqHIGQrHy00J4xwiUTd'
+)
+
+export const bookTour = async (tourId) => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://localhost:8000/api/v1/bookings/checkout-session/${tourId}`
+    )
+    console.log(session)
+    // 2) Create checkout form + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    })
+  } catch (err) {
+    console.log(err)
+    showAlert('error', err)
+  }
+}
